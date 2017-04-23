38b3a834bf858bf9e0fc8e0ae1892343afe5b114
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-04-23 09:22:09 -0300

    Organize imports

--- src/com/paninicms/Panini.java
+++ src/com/paninicms/Panini.java
@@ -2,11 +2,8 @@ package com.paninicms;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
